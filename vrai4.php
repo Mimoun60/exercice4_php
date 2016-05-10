2d653dcb5a36cd4da9b4a8c9b11bf5614fe1a804
Mimoun60 <mimoun.boulashoub@bbox.fr>
2016-05-10 10:50:33 +0200

    exo 4 php

--- vrai4.php
+++ vrai4.php
@@ -7,10 +7,13 @@
 <body>
 
   <?php
-  for ($i=1; $i <= 100; $i++) {
-    echo(($i % 3 == 0 && $i % 5 == 0) ? "FizzBuzz":(($i % 3 == 0) ?"Fizz":($i % 5 == 0) ? "Buzz" : $i))."\n";
+  for ($i=1;$i<=100;$i++){
+    if($i %3 ==0 && $i %5==0){
+      echo "fizzbuzz";
+    }elseif ($i %3 == 0){
+      echo "fizz";
+    }elseif ($i %5 == 0){
+      echo "Buzz";
+    }else echo $i;
   }
   ?>
-</body>
-
-</html>
